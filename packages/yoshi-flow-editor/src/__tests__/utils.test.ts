4bc9391c74aacef7936b6cf5f082623880eb7ecd
Tzachi Azo <tzachia@wix.com>
2020-06-15 10:33:31 +0300

    EP-1009|added support for appBuilderUrl (#2511)
    * EP-1009|added support for appBuilderUrl
    
    * Merge with master and fix lint
    
    Co-authored-by: Artem Yavorsky <info@yavorsky.org>
    Co-authored-by: Artem Yavorsky <aqson@me.com>

--- packages/yoshi-flow-editor/src/__tests__/utils.test.ts
+++ packages/yoshi-flow-editor/src/__tests__/utils.test.ts
@@ -38,7 +38,7 @@ describe('addOverrideQueryParamsWithModel', () => {
       },
       { cdnUrl, serverUrl },
     );
-    const urlWithParams = overrideParams('https://mysite.com');
+    const urlWithParams = overrideParams('https://mysite.com', 'editorUrl');
 
     expect(urlWithParams).toBe(
       `https://mysite.com/?tpaWidgetUrlOverride=WIDGET_ID=${serverUrl}/editor/comp&tpaSettingsUrlOverride=WIDGET_ID=${serverUrl}/settings/comp&widgetsUrlOverride=WIDGET_ID=${cdnUrl}compViewerWidget.bundle.js&viewerPlatformOverrides=APP_DEF_ID=${cdnUrl}viewerScript.bundle.js&editorScriptUrlOverride=APP_DEF_ID=${cdnUrl}editorScript.bundle.js&overridePlatformBaseUrls=APP_DEF_ID={"staticsBaseUrl":"${cdnUrl}"}`,
@@ -82,7 +82,7 @@ describe('addOverrideQueryParamsWithModel', () => {
       },
       { cdnUrl, serverUrl },
     );
-    const urlWithParams = overrideParams('https://mysite.com');
+    const urlWithParams = overrideParams('https://mysite.com', 'editorUrl');
 
     expect(urlWithParams).toBe(
       `https://mysite.com/?tpaWidgetUrlOverride=COMP_WIDGET_ID=${serverUrl}/editor/comp,PAGE_WIDGET_ID=${serverUrl}/editor/page&tpaSettingsUrlOverride=COMP_WIDGET_ID=${serverUrl}/settings/comp,PAGE_WIDGET_ID=${serverUrl}/settings/page&widgetsUrlOverride=COMP_WIDGET_ID=${cdnUrl}compViewerWidget.bundle.js,PAGE_WIDGET_ID=${cdnUrl}pageViewerWidget.bundle.js&viewerPlatformOverrides=APP_DEF_ID=${cdnUrl}viewerScript.bundle.js&editorScriptUrlOverride=APP_DEF_ID=${cdnUrl}editorScript.bundle.js&overridePlatformBaseUrls=APP_DEF_ID={"staticsBaseUrl":"${cdnUrl}"}`,
@@ -117,10 +117,78 @@ describe('addOverrideQueryParamsWithModel', () => {
       },
       { cdnUrl, serverUrl },
     );
-    const urlWithParams = overrideParams('https://mysite.com');
+    const urlWithParams = overrideParams('https://mysite.com', 'editorUrl');
 
     expect(urlWithParams).toBe(
       `https://mysite.com/?tpaWidgetUrlOverride=WIDGET_ID=${serverUrl}/editor/comp&tpaSettingsUrlOverride=WIDGET_ID=${serverUrl}/settings/comp&widgetsUrlOverride=WIDGET_ID=${cdnUrl}compViewerWidget.bundle.js&viewerPlatformOverrides=APP_DEF_ID=${cdnUrl}viewerScript.bundle.js&editorScriptUrlOverride=APP_DEF_ID=https://localhost:5005/editorScript.bundle.js&overridePlatformBaseUrls=APP_DEF_ID={"staticsBaseUrl":"${cdnUrl}"}`,
     );
+  });
+
+  it('use only supported query params for appBuilder', () => {
+    const overrideParams = overrideQueryParamsWithModel(
+      {
+        appName: 'app',
+        projectName: 'some-project',
+        artifactId: '7891',
+        urls: {},
+        editorEntryFileName: null,
+        viewerEntryFileName: 'a/b',
+        appDefId: 'APP_DEF_ID',
+        experimentsConfig: {
+          scope: 'test-scope',
+        },
+        sentry: null,
+        components: [
+          {
+            name: 'comp',
+            id: 'WIDGET_ID',
+            type: OOI_WIDGET_COMPONENT_TYPE,
+            widgetFileName: 'proj/comp/Widget.ts',
+            viewerControllerFileName: 'proj/comp/controller.ts',
+            editorControllerFileName: null,
+            settingsFileName: 'proj/comp/settings.ts',
+          },
+        ],
+      },
+      { cdnUrl, serverUrl },
+    );
+    const urlWithParams = overrideParams('https://mysite.com', 'appBuilderUrl');
+
+    expect(urlWithParams).toBe(
+      `https://mysite.com/?viewerPlatformOverrides=APP_DEF_ID=${cdnUrl}viewerScript.bundle.js`,
+    );
+  });
+
+  it('should not use any override if origin not recognaized', () => {
+    const overrideParams = overrideQueryParamsWithModel(
+      {
+        appName: 'app',
+        artifactId: '7891',
+        urls: {},
+        editorEntryFileName: null,
+        projectName: 'some-project',
+        viewerEntryFileName: 'a/b',
+        appDefId: 'APP_DEF_ID',
+        experimentsConfig: {
+          scope: 'test-scope',
+        },
+        sentry: null,
+        components: [
+          {
+            name: 'comp',
+            id: 'WIDGET_ID',
+            type: OOI_WIDGET_COMPONENT_TYPE,
+            widgetFileName: 'proj/comp/Widget.ts',
+            viewerControllerFileName: 'proj/comp/controller.ts',
+            editorControllerFileName: null,
+            settingsFileName: 'proj/comp/settings.ts',
+          },
+        ],
+      },
+      { cdnUrl, serverUrl },
+    );
+    const urlWithParams = overrideParams('https://mysite.com', 'FAKE');
+
+    expect(urlWithParams).toBe(`https://mysite.com/`);
   });
 });
