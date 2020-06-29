515756b0d44924c00342de1c1e33cf21f9d0cf45
tzachbon <tzachb@wix.com>
2020-06-29 10:05:28 +0300

    test: mock console.warn

--- test/typescript/features/jest-preset-errors/__tests__/simple.e2e.tsx
+++ test/typescript/features/jest-preset-errors/__tests__/simple.e2e.tsx
@@ -1,16 +1,36 @@
-it('should pass', async () => {
-  const waitForRequest = () =>
+describe('Puppeteer errors and logs: ', async () => {
+  let originalConsole: Console;
+  beforeEach(() => {
+    originalConsole = global.console;
+  });
+
+  afterEach(() => {
+    global.console = originalConsole;
+  });
+
+  const waitForFakeUrl = () =>
     new Promise((res, rej) => {
       const timeout = setTimeout(rej, 5000);
 
       page.on('requestfinished', (req) => {
         if (req.url().includes('fake-url')) {
           clearTimeout(timeout);
-          rej(req.url());
+          res(req.url());
         }
       });
     });
 
-  await page.goto('http://localhost:3100');
-  await waitForRequest();
+  it('should warn user when request gets 404 or 503', async () => {
+    const warnMessage =
+      'Request failed or not found. url: http://localhost:3100/fake-url, status code: 404, method: GET';
+
+    // @ts-ignore
+    global.console = { warn: jest.fn() };
+
+    await page.goto('http://localhost:3100');
+
+    await waitForFakeUrl();
+
+    expect(global.console.warn).toBeCalledWith(warnMessage);
+  });
 });
--- test/typescript/features/jest-preset-errors/e2e.test.ts
+++ test/typescript/features/jest-preset-errors/e2e.test.ts
@@ -10,14 +10,7 @@ jest.setTimeout(40000);
 describe.each(['dev'] as const)('show browser errors in terminal', (mode) => {
   it('run tests', async () => {
     await scripts[mode](async () => {
-      const warnMessage =
-        'Request failed or not found. url: http://localhost:3100/fake-url, status code: 404, method: GET';
-
-      try {
-        await scripts.test(mode);
-      } catch (e) {
-        expect(e.message).toMatch(warnMessage);
-      }
+      await scripts.test(mode);
     });
   });
 });
